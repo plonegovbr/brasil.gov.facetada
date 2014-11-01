3ff7b688ee98b1e6599394fa075251e4c1e469d2
Érico Andrei <erico@simplesconsultoria.com.br>
2014-11-01 11:45:41 +0000

    Ocultaremos os produtos do QuickInstaller também

--- src/brasil/gov/facetada/config.py
+++ src/brasil/gov/facetada/config.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
-
-from Products.CMFPlone.interfaces import INonInstallable
+from Products.CMFPlone import interfaces as st_interfaces
+from Products.CMFQuickInstallerTool import interfaces as qi_interfaces
 from zope.interface import implements
 
 PROJECTNAME = 'brasil.gov.facetada'
 
 
+class HiddenProducts(object):
+    implements(qi_interfaces.INonInstallable)
+
+    def getNonInstallableProducts(self):
+        return [
+        ]
+
+
 class HiddenProfiles(object):
-    implements(INonInstallable)
+    implements(st_interfaces.INonInstallable)
 
     def getNonInstallableProfiles(self):
         return [
             u'brasil.gov.facetada:uninstall',
-            u'brasil.gov.facetada.upgrades.v1010:default'
         ]
