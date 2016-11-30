e232976d0676c93883a278743f6a52cab1fd1699
hvelarde <hector.velarde@gmail.com>
2016-11-30 11:19:34 -0200

    Back to development: 1.0b2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '1.0b1'
+version = '1.0b2.dev0'
 description = u'Suporte à busca multifacetada para Portal Padrão'
 long_description = (
     open('README.rst').read() + '\n' +
